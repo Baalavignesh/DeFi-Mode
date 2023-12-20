243514a40f64f6a87422724189d6691416de8a46
baalavignesh21 <baalavignesha@presidio.com>
2023-12-20 20:30:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    colors: {
+      'primary': '#D6FF00',
+      'secondary': '#346DDB',
+      'secondary-2':'#307F98',
+      'custom-grey': '#22272E',
+      'custom-white':'#f2f2f2',
+      'custom-black': '#14181C',
+    },
+    extend: {},
+
+  },
+
+  plugins: [],
+
+};
