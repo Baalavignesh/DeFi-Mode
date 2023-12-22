879f35694ad14364c7c2abdf6eeefb99ccc32f5f
mithileshchellappan <mithilesh153@gmail.com>
2023-12-22 13:05:21 +0530

    added metamask login

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "./init.js"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -5,13 +6,23 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/store.ts";
 import { PersistGate } from "redux-persist/integration/react"; 
+import { MetaMaskProvider } from '@metamask/sdk-react';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <MetaMaskProvider debug={false} sdkOptions={{
+      dappMetadata: {
+        name: "ModeStart",
+        url: window.location.host,
+      }
+      // Other options
+    }}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
+    </MetaMaskProvider>
   </React.StrictMode>
 );
--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -3,45 +3,77 @@ import { setTokens } from "../../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Box, Fade, LinearProgress } from "@mui/material";
+import { useSDK } from "@metamask/sdk-react";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
+  const [account, setAccount] = useState<string>();
+  const { sdk } = useSDK();
   const [loading, setLoading] = useState<boolean>(false);
-  const handleLogin = () => {
-    console.log("set");
-    dispatch(
-      setTokens({
-        accessToken: "access_token",
-        refreshToken: "refresh_token",
-        idToken: "id_token",
-      })
-    );
-    navigate("/dashboard");
+  const chainId = `0x${Number(919).toString(16)}`
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const accounts = await sdk?.connect();
+      await window.ethereum?.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: chainId }]
+      });
+      setAccount((accounts as string[] | undefined)?.[0] ?? "");
+
+      dispatch(
+        setTokens({
+          accessToken: "access_token",
+          refreshToken: "refresh_token",
+          idToken: "id_token",
+        })
+      );
+      setLoading(false);
+      navigate("/dashboard");
+    } catch (err: any) {
+      if (err?.code === 4902) {
+        console.log('inside')
+        await window.ethereum?.request({
+          method: "wallet_addEthereumChain",
+          params: [{
+            chainName: "Mode Testnet", chainId: chainId, 
+            blockExplorerUrls: ['https://bridge.mode.network/'],
+            rpcUrls: ['https://sepolia.mode.network/'],
+             nativeCurrency: {
+              name: "Ethereum",
+              symbol: "ETH",
+              decimals: 18,
+            }
+          },
+
+          ],
+        })
+        setLoading(false);
+        navigate("/dashboard");
+      }
+      console.warn(`failed to connect..`, err);
+    }
+
   };
 
   useEffect(() => {
     console.log("login screen");
   }, []);
 
-
-
-
-
   return (
     <div className="min-h-screen">
 
       <Fade in={true} timeout={1000}>
         <div className="flex flex-col justify-center items-center h-screen gap-8">
-    
+
 
           <h1>Welcome to ModeStart</h1>
           <button
             className="p-4 rounded-sm bg-primary text-custom-black"
             onClick={handleLogin}
           >
-            Login with ETH Auth
+            Login with Metamask
           </button>
         </div>
       </Fade>
